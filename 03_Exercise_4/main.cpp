afd982955b511f682d0dd51def07f23034b0d751
mrigakshipandey <mrigakshipandey@gmail.com>
2025-02-09 16:41:07 +0100

    Exercise 4 (Single Memory Bank)

--- 03_Exercise_4/main.cpp
+++ 03_Exercise_4/main.cpp
@@ -7,41 +7,40 @@
 // Toplevel:
 // TODO
 SC_MODULE(toplevel){
-    transition<1,2> t1; 
-    transition<2,1> t2;
-    transition<1,1> t3;
-    place<1,1> p1, p2, p3, p4;
+    transition<1,1> ACT, RD, PRE, WR;
+    place<1,1> IDLE, ACTIVE;
 
        
-    SC_CTOR(toplevel): t1("t1"), t2("t2"), t3("t3"), p1(1), p2(0), p3(0), p4(0) //  Petri Net (PN) as described in Fig. 2
+    SC_CTOR(toplevel): ACT("ACT"), RD("RD"), PRE("PRE"), WR("WR"), IDLE(1), ACTIVE(0) //  Petri Net (PN) as described in Fig. 3
     {
         SC_THREAD(process);
 
-        // Petri Net (PN) as described in Fig. 1
-        t2.out.bind(p1);
-        t1.in.bind(p1);
+        // Petri Net (PN) as described in Fig. 3
+        PRE.out.bind(IDLE);
+        ACT.in.bind(IDLE);
 
-        t1.out.bind(p2);
-        t2.in.bind(p2);
-
-        t1.out.bind(p3);
-        t3.in.bind(p3);
-
-        t3.out.bind(p4);
-        t2.in.bind(p4);
-
+        ACT.out.bind(ACTIVE);
+        RD.out.bind(ACTIVE);
+        WR.out.bind(ACTIVE);
+        PRE.in.bind(ACTIVE);
+        RD.in.bind(ACTIVE);
+        WR.in.bind(ACTIVE);
     }
 
     void process(){
         while(true) {
             wait(10, SC_NS);
-            t1.fire();
+            ACT.fire();
             wait(10, SC_NS);
-            t2.fire();
+            ACT.fire();
             wait(10, SC_NS);
-            t3.fire();
+            RD.fire();
             wait(10, SC_NS);
-            t2.fire();
+            WR.fire();
+            wait(10, SC_NS);
+            PRE.fire();
+            wait(10, SC_NS);
+            ACT.fire();
             sc_stop();
         }
     }
